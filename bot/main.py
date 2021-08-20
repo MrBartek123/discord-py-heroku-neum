f5be875e1b106a5ebf18190d96c86545eb4624a7
MrBartek123 <77856176+MrBartek123@users.noreply.github.com>
2021-08-20 16:51:34 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,22 @@
 import os
 from discord.ext import commands
+import time
+import discord
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="n!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+    discord.Activity(name="Neum Bugs", type=5)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("pong")
+    before = time.monotonic()
+    message = await ctx.send("Pong!")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"Pong!  `{int(ping)}ms`")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
