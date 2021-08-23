bca83924808aee98f069ac7a096a446a7d258d8a
MrBartek123 <77856176+MrBartek123@users.noreply.github.com>
2021-08-23 18:46:43 +0200

    haha yes

---
+++ currencyAPI/commands.py
@@ -0,0 +1,25 @@
+import random
+
+import requests
+import os
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_components import create_button, create_actionrow
+from discord_slash.model import ButtonStyle
+from discord.utils import get
+import psycopg2
+import pickledb
+
+
+## SETUP
+db = pickledb.load('database.db', False)
+
+def work(username):
+    moneyWithout = random.randint(1, 255)
+    moneyWithBonus = moneyWithout + db.get(f'{username.name}Bonus')
+    db.set(f'{username.name}Points', moneyWithBonus)
+    return moneyWithBonus
+
+def balance(member):
+    return f"{member.mention} balance is {db.get(f'{member.name}Points')} Points"
+def bonusBalance(member):
+    return f"{member.mention}, your points bonus is {db.get(f'{member.name}Bonus')}"
