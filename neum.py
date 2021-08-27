ce0fc0820cb50487a41f1629819456f565dc1ad8
MrBartek123 <77856176+MrBartek123@users.noreply.github.com>
2021-08-27 20:33:29 +0200

    haha yes

--- neum.py
+++ neum.py
@@ -422,7 +422,7 @@ async def spaghetti(ctx):
 @botM.command()
 async def fakeWarn(ctx, member: discord.Member):
     channel = await member.create_dm()
-    embed = discord.Embed(description=f"Warning from {ctx.guild.name}:\nReason: Breaking rules\nLearn more about [warns](https://neum-discord.herokuapp.com/why.html?page=Warns)")
+    embed = discord.Embed(description=f"Warning from {ctx.guild.name}:\nReason: Breaking rules\nLearn more about [warns](https://neum-discord.herokuapp.com/warn)")
     await channel.send(embed=embed)
     await ctx.send(f"<a:yes:878700406048432238>| ||Fake|| Warned {member}")
 if __name__ == "__main__":
